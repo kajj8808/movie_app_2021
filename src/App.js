12dfd5f70ac1413f5fc2d45751ab758b4567b3ac
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2021-03-01 06:13:25 +0900

    day.1.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+const cards = [
+  {
+    id : 1,
+    name : "Initial",
+    image : "https://static.wikia.nocookie.net/princess-connect/images/a/aa/6KyaruUB_%28gif%29.gif",
+    rating : 9.0
+  },
+  {
+    id : 2,
+    name : "Summer",
+    image : "https://static.wikia.nocookie.net/princess-connect/images/1/13/FullArtSummerKyaru.png",
+    rating : 9.5
+  },
+  {
+    id : 3,
+    name : "New Year",
+    image : "https://static.wikia.nocookie.net/princess-connect/images/1/10/FullArtNewYearKyaru.png",
+    rating : 10
+  },
+  {
+    id : 4,
+    name : "Princess",
+    image : "https://static.wikia.nocookie.net/princess-connect/images/2/2a/FullArtPKyaru.png",
+    rating : 9.5
+  }
+]
+
+const renderCards = (card) =>{
+  return <Karyl key = {card.id} name = {card.name} image = {card.image} rating = {card.rating} />
+}
+
+const Karyl = ({ name , image , rating }) =>{
+  return(
+    <div>
+      <h1> Karyl / {name} </h1>
+      <h4> {rating} </h4>
+      <img src = {image} alt = {name}></img>
+
+    </div>
+  )
+}
+
+Karyl.propTypes = {
+  name : PropTypes.string.isRequired,
+  image : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cards.map(renderCards)}
     </div>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* root id 에 App 의 App 를 넣는거. kayel 를넣어도 작동함 */
+/* 소스 파일을보면 그냥 빈 div id root 가 있음!. */
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
